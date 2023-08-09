f0746674fc3b60d01606ed2cf689503c8ec207a4
Hollis_Ho <he_wenzhi@126.com>
2023-08-09 18:26:13 +0800

    ini

---
+++ src/BaseClient.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace hollisho\httpclient;
+
+
+class BaseClient
+{
+    use HasHttpRequests { request as performRequest; }
+
+    /**
+     * @var string
+     */
+    protected $baseUri;
+
+    protected $guzzleHandler;
+
+    /**
+     * GET request.
+     * @param string $url
+     * @param array $query
+     * @return \Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function httpGet(string $url, array $query = [])
+    {
+        return $this->request($url, 'GET', ['query' => $query]);
+    }
+
+    /**
+     * POST request.
+     * @param string $url
+     * @param array $data
+     * @return \Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function httpPost(string $url, array $data = [])
+    {
+        return $this->request($url, 'POST', ['form_params' => $data]);
+    }
+
+    /**
+     * JSON request.
+     * @param string $url
+     * @param array $data
+     * @param array $query
+     * @return \Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function httpPostJson(string $url, array $data = [], array $query = [])
+    {
+        return $this->request($url, 'POST', ['query' => $query, 'json' => $data]);
+    }
+
+    /**
+     * Upload file.
+     * @param string $url
+     * @param array $files
+     * @param array $form
+     * @param array $query
+     * @return \Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function httpUpload(string $url, array $files = [], array $form = [], array $query = [])
+    {
+        $multipart = [];
+
+        foreach ($files as $name => $path) {
+            $multipart[] = [
+                'name' => $name,
+                'contents' => fopen($path, 'r'),
+            ];
+        }
+
+        foreach ($form as $name => $contents) {
+            $multipart[] = compact('name', 'contents');
+        }
+
+        return $this->request($url, 'POST', ['query' => $query, 'multipart' => $multipart, 'connect_timeout' => 30, 'timeout' => 30, 'read_timeout' => 30]);
+    }
+}
